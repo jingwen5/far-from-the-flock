8616c9f59af5e6bacefd6c41b06edb22c4d5469c
jingwen5 <jingwen5@andrew.cmu.edu>
2025-04-20 04:34:37 -0400

    Added Terrain Assets
    Made the terrain sprites such as grass texture, trees, etc.
    Also made player character sprite and movement script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float groundDist;
+
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.linearVelocity = moveDir * speed;
+
+        if (x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+}
